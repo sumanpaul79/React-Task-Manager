891adfb7e447f464d2424272b6bb619a41ad322f
root <root@ip-172-31-37-93.eu-west-2.compute.internal>
2020-06-16 01:58:41 +0000

    Updates

---
+++ precache-manifest.3c145d596197421b18b6e644d536afa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f549958acd2df9e7bf487162c85a11",
+    "url": "/React-Task-Manager/index.html"
+  },
+  {
+    "revision": "40912292b06f92d0b7d4",
+    "url": "/React-Task-Manager/static/css/main.099b918d.chunk.css"
+  },
+  {
+    "revision": "58c4682a155b48f97300",
+    "url": "/React-Task-Manager/static/js/2.c0f77a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Task-Manager/static/js/2.c0f77a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40912292b06f92d0b7d4",
+    "url": "/React-Task-Manager/static/js/main.c0f3adba.chunk.js"
+  },
+  {
+    "revision": "75c0c7098d3e16dc5189",
+    "url": "/React-Task-Manager/static/js/runtime-main.5789c9eb.js"
+  }
+]);
